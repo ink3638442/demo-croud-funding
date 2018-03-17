dc22a008127fff97fec4f7e3f90c88624db7d217
akihiro nagashima <akihiro.nagashima@gmail.com>
2018-03-17 14:59:56 +0900

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
-  def hello
-    render html: "hello, world!"
-  end
+  protect_from_forgery with: :exception
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+
+  devise_for :users
+  root "home#index"
 end
